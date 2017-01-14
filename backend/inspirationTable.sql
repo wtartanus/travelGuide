ed963e47fa758334a088411487a1381631fd3dc8
Wojtek Tartanus <wojtek.tartanus@airts.co.uk>
2017-01-14 21:17:19 +0000

    Few changes to the table, and collected data for inspirations

--- backend/inspirationTable.sql
+++ backend/inspirationTable.sql
@@ -2,35 +2,38 @@ CREATE TABLE inspirations (
 	id SERIAL4 primary key,
 	city VARCHAR(255),
 	country VARCHAR(255),
-	description VARCHAR(255)
-)
+	description TEXT,
+	history TEXT
+);
 
 CREATE TABLE photos (
     id SERIAL4 primary key,
     city_id INT4 references inspirations (id) ON DELETE CASCADE,
     link VARCHAR(255)
-)
+);
 
 CREATE TABLE temperatures (
     id SERIAL4 primary key,
     city_id INT4 references inspirations (id) ON DELETE CASCADE,
-    Jan INT 4,
-    Feb INT 4,
-    Mar INT 4,
-    Apr INT 4,
-    May INT 4,
-    June INT 4,
-    July INT 4,
-    Aug  INT 4,
-    Sept INT 4,
-    Oct INT 4,
-    Nov INT 4,
-    Dec INT 4
-)
+    Jan INT4,
+    Feb INT4,
+    Mar INT4,
+    Apr INT4,
+    May INT4,
+    June INT4,
+    July INT4,
+    Aug  INT4,
+    Sept INT4,
+    Oct INT4,
+    Nov INT4,
+    Dec INT4
+);
 
-CREATE TABLE activities (
-
-)
-
-
- things to do
+CREATE TABLE activities1 (
+	id SERIAL4 primary key,
+	city_id INT4 references inspirations (id) ON DELETE CASCADE,
+	name VARCHAR(255),
+	description TEXT,
+	address TEXT,
+	photo_link TEXT
+);
